176ac11e88f96a56613b0069ee11d8c7c554e75d
“ValerioG.” <i@valeriogoncharov.ru>
2025-04-30 13:21:55 +0300

    Finished second excercise

---
+++ Excercise2.Accounts/Excercise2.Accounts/Excercise2.Accounts.cpp
@@ -0,0 +1,29 @@
+﻿
+
+#include <iostream>
+#include <string>
+
+struct Account
+{
+    std::string name;
+    int number;
+    int ballance;
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    Account userAccount;
+    std::cout << "Введите номер счёта:\n";
+    std::cin >> userAccount.number;
+    std::cout << "Введите имя владельца:\n";
+    std::cin >> userAccount.name;
+    std::cout << "Введите новый баланс:\n";
+    std::cin >> userAccount.ballance;
+    std::cout << "Ваш счёт: " << userAccount.name << "," << userAccount.number << "," << userAccount.ballance << std::endl;
+
+    return 0;
+
+}
+
