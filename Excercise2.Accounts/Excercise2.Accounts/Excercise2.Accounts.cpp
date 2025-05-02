91b95f8465e16650a4a2c94dc0085f162a872535
“ValerioG.” <i@valeriogoncharov.ru>
2025-05-02 14:00:20 +0300

    Reforged third excercise

--- Excercise2.Accounts/Excercise2.Accounts/Excercise2.Accounts.cpp
+++ Excercise2.Accounts/Excercise2.Accounts/Excercise2.Accounts.cpp
@@ -7,8 +7,10 @@ struct Account
 {
     std::string name;
     int number;
-    int ballance;
+    double balance;
 };
+
+void changeBalance(Account& account, double newBalance);
 
 int main()
 {
@@ -19,11 +21,22 @@ int main()
     std::cin >> userAccount.number;
     std::cout << "Введите имя владельца:\n";
     std::cin >> userAccount.name;
+    std::cout << "Введите баланс:\n";
+    std::cin >> userAccount.balance;
+
+    double newBalance;
     std::cout << "Введите новый баланс:\n";
-    std::cin >> userAccount.ballance;
-    std::cout << "Ваш счёт: " << userAccount.name << "," << userAccount.number << "," << userAccount.ballance << std::endl;
+    std::cin >> newBalance;
+
+    changeBalance(userAccount, newBalance);
+
+    std::cout << "Ваш счёт: " << userAccount.name << "," << userAccount.number << "," << userAccount.balance << std::endl;
 
     return 0;
 
 }
 
+void changeBalance(Account& account, double newBalance)
+{
+    account.balance = newBalance;
+}
